389ad1dc0937c180a3be02603c98c1b54caa1df2
YOURiSST <yurikonst04@mail.ru>
2021-12-26 15:43:25 +0300

    Create мосты.cpp

---
+++ алгосы/мосты.cpp
@@ -0,0 +1,73 @@
+
+struct Edge {
+    int from, to;
+    Edge(int from_, int to_) {
+        from = from_;
+        to = to_;
+    }  
+};
+
+vector<int> g[MAXN];
+int h[MAXN];
+int hup[MAXN];
+bool used[MAXN];
+vector<Edge> bridges;
+
+void dfs(int v, int p) {
+    h[v] = h[p] + 1;
+    hup[v] = h[v];
+    for (auto & to : g[v]) {
+        if (to == p) {
+            continue;
+        } else if (!used[to]) {
+            dfs(to, v);
+            hup[v] = min(hup[v], hup[to]);
+            if (hup[to] > h[v]) {
+                bridges.eb(v, to);
+            } 
+        } else {
+            hup[v] = min(hup[v], h[to]);
+        }
+    }
+}
+
+
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int from, to; cin >> from >> to; from--; to--;
+        g[from].eb(to);
+        g[to].eb(from);
+    }
+    for (int i = 0; i < n; ++i) {
+        if (!used[i]) {
+            dfs(i, i);
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
